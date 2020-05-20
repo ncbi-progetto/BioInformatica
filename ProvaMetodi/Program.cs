f59ae39c7721b43556122fcf5ea2713f2e108638
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2020-05-20 20:50:27 +0200

    Creazione File. Primo Metodo : lunghezza stringa e prova con Console App.

---
+++ ProvaMetodi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProvaMetodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sensibile = ClasseBioInformatica.Metodi.LetturaSensibile();
+            long lunghezzas = ClasseBioInformatica.Metodi.LunghezzaSensibile();
+            Console.WriteLine(sensibile);
+            Console.WriteLine("è lunga " + lunghezzas);
+        }
+    }
+}
